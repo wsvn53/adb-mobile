4a7bf1693e3cdede36c1b21f242c5a75299076e8
ethan <wsvn53@gmail.com>
2022-05-31 22:06:42 +0800

    Finish build brotli/protobuf libraries

---
+++ porting/scripts/make-brotli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. $(dirname $0)/defines.sh;
+
+cmake_root=$SOURCE_ROOT/external/brotli/out;
+
+# Clean built products
+[[ -d "$cmake_root" ]] && rm -rfv "$cmake_root";
+mkdir -pv "$cmake_root";
+
+cd "$cmake_root";
+
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE -DPLATFORM=$PLATFORM  \
+	-DDEPLOYMENT_TARGET=$DEPLOYMENT_TARGET -DBROTLI_BUNDLED_MODE=1;
+cmake --build . --config Release --parallel 8 \
+	--target brotlidec-static --target brotlienc-static --target brotlicommon-static;
+find . -name "*.a" -exec cp -av {} $FULL_OUTPUT \;
---
+++ porting/scripts/make-protobuf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. $(dirname $0)/defines.sh;
+
+cmake_root=$SOURCE_ROOT/external/protobuf/out;
+
+# Clean built products
+[[ -d "$cmake_root" ]] && rm -rfv "$cmake_root";
+mkdir -pv "$cmake_root";
+
+cd "$cmake_root";
+
+which protoc || {
+	echo "** ERROR: protoc not installed";
+	exit 1;
+}
+protoc_version=$(protoc --version | cut -d' ' -f2 | cut -d. -f1 -f2).x;
+
+# Switch to version
+git checkout $protoc_version;
+
+# Copy CMakeLists.txt to source folder
+cp $SOURCE_ROOT/porting/cmake/protobuf.CMakeLists.txt $SOURCE_ROOT/external/protobuf/CMakeLists.txt;
+
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE -DPLATFORM=$PLATFORM -DDEPLOYMENT_TARGET=$DEPLOYMENT_TARGET;
+cmake --build . --target protobuf --config Release --parallel 8;
+find . -name "*.a" -exec cp -av {} $FULL_OUTPUT \;
